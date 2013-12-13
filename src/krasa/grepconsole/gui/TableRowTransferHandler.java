6288756e9b741a638e81364bab1d63ac1ce68fc3
Vojtech Krasa <vojta.krasa@gmail.com>
2013-12-13 13:33:54 +0100

    #12 fixed settings table drag&drop

--- src/krasa/grepconsole/gui/TableRowTransferHandler.java
+++ src/krasa/grepconsole/gui/TableRowTransferHandler.java
@@ -1,16 +1,15 @@
 package krasa.grepconsole.gui;
 
-import java.awt.*;
-import java.awt.datatransfer.DataFlavor;
-import java.awt.datatransfer.Transferable;
-import java.awt.dnd.DragSource;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.util.ui.ListTableModel;
 
 import javax.activation.ActivationDataFlavor;
 import javax.activation.DataHandler;
 import javax.swing.*;
-
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.util.ui.EditableModel;
+import java.awt.*;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.dnd.DragSource;
 
 public class TableRowTransferHandler extends TransferHandler {
 	private static final Logger log = Logger.getInstance(TableRowTransferHandler.class.getName());
@@ -41,6 +40,7 @@ public class TableRowTransferHandler extends TransferHandler {
 		return TransferHandler.COPY_OR_MOVE;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public boolean importData(TransferHandler.TransferSupport info) {
 		JTable target = (JTable) info.getComponent();
@@ -53,9 +53,14 @@ public class TableRowTransferHandler extends TransferHandler {
 		try {
 			Integer rowFrom = (Integer) info.getTransferable().getTransferData(localObjectFlavor);
 			if (rowFrom != -1 && rowFrom != index) {
-				if (index > rowFrom)
+				final ListTableModel model = (ListTableModel) table.getModel();
+				model.insertRow(index, model.getRowValue(rowFrom));
+				if (rowFrom > index) {
+					rowFrom++;
+				} else {
 					index--;
-				((EditableModel) table.getModel()).exchangeRows(rowFrom, index);
+				}
+				model.removeRow(rowFrom);
 
 				target.getSelectionModel().addSelectionInterval(index, index);
 				return true;
