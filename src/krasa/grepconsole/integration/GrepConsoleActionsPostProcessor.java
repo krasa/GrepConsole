f2ae5ab45e36e07faf2586df1b549b5b89276be0
Vojtech Krasa <vojta.krasa@gmail.com>
2016-08-23 14:58:09 +0200

    #58 actions order

--- src/krasa/grepconsole/integration/GrepConsoleActionsPostProcessor.java
+++ src/krasa/grepconsole/integration/GrepConsoleActionsPostProcessor.java
@@ -46,8 +46,8 @@ public class GrepConsoleActionsPostProcessor extends ConsoleActionsPostProcessor
 	public AnAction[] postProcessPopupActions(@NotNull ConsoleView console, @NotNull AnAction[] actions) {
 		ServiceManager manager = ServiceManager.getInstance();
 		ArrayList<AnAction> anActions = new ArrayList<AnAction>();
-		anActions.add(new AddHighlightAction("Add highlight", "Add highlight for this selected text", null));
 		anActions.add(new OpenGrepConsoleAction("Grep", "Open a new filter/grep console", null));
+		anActions.add(new AddHighlightAction("Add highlight", "Add highlight for this selected text", null));
 		if (manager.isRegistered(console)) {
 			anActions.add(new ShowHideStatisticsConsolePanelAction(console));
 			anActions.add(new ShowHideStatisticsStatusBarPanelAction(console));
