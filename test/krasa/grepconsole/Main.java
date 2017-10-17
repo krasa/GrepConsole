74d9af4d06d11d63ef3aa49bc143bf5dafce1b1e
Vojtěch Krása <vojta.krasa@gmail.com>
2017-10-17 13:21:25 +0200

    test class

---
+++ test/krasa/grepconsole/Main.java
@@ -0,0 +1,11 @@
+package krasa.grepconsole;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("[FATAL].............................");
+		System.out.println("[ERROR].............................");
+		System.out.println("[WARN].............................");
+		System.out.println("[DEBUG].............................");
+		System.out.println("[TRACE].............................");
+	}
+}
