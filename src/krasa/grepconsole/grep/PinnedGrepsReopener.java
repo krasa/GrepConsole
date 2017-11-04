bebd9b6a97a692a67e0938704e71c8ecb48bd25b
Vojtěch Krása <vojta.krasa@gmail.com>
2017-11-04 18:49:27 +0100

    Fixes #93 workaround for synchronization issue with calling com.intellij.execution.impl.ExecutionManagerImpl.getContentManager on a pooled thread - 2 instances are initialized in a strange way

--- src/krasa/grepconsole/grep/PinnedGrepsReopener.java
+++ src/krasa/grepconsole/grep/PinnedGrepsReopener.java
@@ -1,20 +1,20 @@
 package krasa.grepconsole.grep;
 
+import java.lang.ref.WeakReference;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import krasa.grepconsole.utils.FocusUtils;
+
 import com.intellij.execution.ExecutionHelper;
 import com.intellij.execution.impl.ConsoleViewImpl;
 import com.intellij.execution.ui.ConsoleView;
 import com.intellij.execution.ui.RunContentDescriptor;
 import com.intellij.openapi.project.Project;
+import com.intellij.ui.AppUIUtil;
 import com.intellij.util.Alarm;
 import com.intellij.util.SingleAlarm;
-import krasa.grepconsole.utils.FocusUtils;
-
-import javax.swing.*;
-import java.lang.ref.WeakReference;
-import java.lang.reflect.InvocationTargetException;
-import java.util.Collection;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class PinnedGrepsReopener {
 	private final SingleAlarm myUpdateAlarm;
@@ -32,41 +32,38 @@ public class PinnedGrepsReopener {
 				if (project.isDisposed()) {
 					return;
 				}
+				AppUIUtil.invokeOnEdt(() -> {
+					Collection<RunContentDescriptor> descriptors = ExecutionHelper.findRunningConsole(project,
+							dom -> FocusUtils.isSameConsole(dom, consoleView, false));
 
-				Collection<RunContentDescriptor> descriptors =
-						ExecutionHelper.findRunningConsole(project,
-								dom -> FocusUtils.isSameConsole(dom, consoleView, false));
-
-				if (!descriptors.isEmpty()) {
-					if (descriptors.size() == 1) {
-						RunContentDescriptor runContentDescriptor = (RunContentDescriptor) descriptors.toArray()[0];
-						PinnedGrepConsolesState.RunConfigurationRef key = new PinnedGrepConsolesState.RunConfigurationRef(runContentDescriptor.getDisplayName(), runContentDescriptor.getIcon());
-						PinnedGrepConsolesState.Pins state = PinnedGrepConsolesState.getInstance(project).getPins(key);
-						if (state != null && !state.getPins().isEmpty() && consoleView instanceof ConsoleViewImpl) {
-							try {
-								SwingUtilities.invokeAndWait(() -> {
-									if (project.isDisposed()) {
-										return;
-									}
-									try {
-										enabled = false;
-										List<PinnedGrepConsolesState.Pin> list = state.getPins();
-										for (PinnedGrepConsolesState.Pin pin : list) {
-											if (pin.getParentConsoleUUID() == null) {
-												initConsole(pin, (ConsoleViewImpl) consoleView, list);
-											}
+					if (!descriptors.isEmpty()) {
+						if (descriptors.size() == 1) {
+							RunContentDescriptor runContentDescriptor = (RunContentDescriptor) descriptors.toArray()[0];
+							PinnedGrepConsolesState.RunConfigurationRef key = new PinnedGrepConsolesState.RunConfigurationRef(
+									runContentDescriptor.getDisplayName(), runContentDescriptor.getIcon());
+							PinnedGrepConsolesState.Pins state = PinnedGrepConsolesState.getInstance(project).getPins(key);
+							if (state != null && !state.getPins().isEmpty() && consoleView instanceof ConsoleViewImpl) {
+								if (project.isDisposed()) {
+									return;
+								}
+								try {
+									enabled = false;
+									List<PinnedGrepConsolesState.Pin> list = state.getPins();
+									for (PinnedGrepConsolesState.Pin pin : list) {
+										if (pin.getParentConsoleUUID() == null) {
+											initConsole(pin, (ConsoleViewImpl) consoleView, list);
 										}
-									} finally {
-										enabled = true;
 									}
-								});
-							} catch (InterruptedException | InvocationTargetException ignored) {
+								} finally {
+									enabled = true;
+								}
+
 							}
 						}
+					} else if (atomicInteger.incrementAndGet() < 3) {
+						myUpdateAlarm.cancelAndRequest();
 					}
-				} else if (atomicInteger.incrementAndGet() < 3) {
-					myUpdateAlarm.cancelAndRequest();
-				}
+				});
 			}
 
 			public void initConsole(PinnedGrepConsolesState.Pin pin, ConsoleViewImpl parent, List<PinnedGrepConsolesState.Pin> list) {
@@ -81,6 +78,5 @@ public class PinnedGrepsReopener {
 		}, 100, Alarm.ThreadToUse.POOLED_THREAD, project);
 		myUpdateAlarm.request();
 	}
-
 
 }
