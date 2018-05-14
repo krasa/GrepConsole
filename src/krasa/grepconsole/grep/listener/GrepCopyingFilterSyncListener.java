e38093d3ec73b0077c153e87a09fe9634b11db54
Vojtěch Krása <vojta.krasa@gmail.com>
2018-05-14 12:38:44 +0200

    another grep after filtering edge case

--- src/krasa/grepconsole/grep/listener/GrepCopyingFilterSyncListener.java
+++ src/krasa/grepconsole/grep/listener/GrepCopyingFilterSyncListener.java
@@ -6,6 +6,7 @@ import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.progress.ProcessCanceledException;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Key;
+import com.intellij.openapi.util.text.StringUtil;
 import krasa.grepconsole.grep.GrepModel;
 import krasa.grepconsole.grep.OpenGrepConsoleAction;
 import krasa.grepconsole.model.Profile;
@@ -14,16 +15,18 @@ import krasa.grepconsole.utils.Utils;
 import org.apache.commons.lang.StringUtils;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
+
 public class GrepCopyingFilterSyncListener implements GrepCopyingFilterListener {
 	private static final Logger log = Logger.getInstance(GrepCopyingFilterSyncListener.class);
-	                                 
+
 	private final OpenGrepConsoleAction.LightProcessHandler myProcessHandler;
 	private final Project project;
 	private volatile GrepModel.Matcher matcher;
 	private volatile Profile profile;
 	private volatile boolean showLimitNotification = true;
 	private ThreadLocal<String> previousIncompleteToken = new ThreadLocal<>();
-	
+
 	public GrepCopyingFilterSyncListener(OpenGrepConsoleAction.LightProcessHandler myProcessHandler, Project project, Profile profile) {
 		this.myProcessHandler = myProcessHandler;
 		this.project = project;
@@ -41,52 +44,58 @@ public class GrepCopyingFilterSyncListener implements GrepCopyingFilterListener 
 	}
 
 	@Override
-	public void process(String s, ConsoleViewContentType type) {
-		if (matcher == null || StringUtils.isEmpty(s)) {
+	public void process(String text, ConsoleViewContentType type) {
+		if (matcher == null || StringUtils.isEmpty(text)) {
 			return;
 		}
+		List<String> split = StringUtil.split(text, "\n", false, false);
+		for (int i = 0; i < split.size(); i++) {
+			String s = split.get(i);
 
-		if (!s.endsWith("\n")) {
-			if (previousIncompleteToken.get() != null) {
-				previousIncompleteToken.set(previousIncompleteToken.get() + s);
-			} else {
-				previousIncompleteToken.set(s);
-			}
-			return;
-		}
-
-		if (previousIncompleteToken.get() != null) {
-			s = previousIncompleteToken.get() + s;
-			previousIncompleteToken.set(null);
-		}
-
-		Key stdout = ProcessOutputTypes.STDOUT;
-		if (type == ConsoleViewContentType.ERROR_OUTPUT) {
-			stdout = ProcessOutputTypes.STDERR;
-		} else if (type == ConsoleViewContentType.SYSTEM_OUTPUT) {
-			stdout = ProcessOutputTypes.SYSTEM;
-		}
-
-		String substring = profile.limitInputGrepLength_andCutNewLine(s);
-		CharSequence charSequence = profile.limitProcessingTime(substring);
-		try {
-			if (matcher.matches(charSequence)) {
-				if (!s.endsWith("\n")) {
-					s = s + "\n";
+			if (!s.endsWith("\n")) {
+				if (previousIncompleteToken.get() != null) {
+					previousIncompleteToken.set(previousIncompleteToken.get() + s);
+				} else {
+					previousIncompleteToken.set(s);
 				}
-				myProcessHandler.notifyTextAvailable(s, stdout);
+				continue;
 			}
-		} catch (ProcessCanceledException e) {
-			String message = "Grep to a subconsole took too long, aborting to prevent input freezing.\n"
-					+ "Consider changing following settings: 'Match only first N characters on each line' or 'Max processing time for a line'\n"
-					+ "Matcher: " + matcher + "\n" + "Line: " + Utils.toNiceLineForLog(substring);
-			if (showLimitNotification) {
-				showLimitNotification = false;
-				Notifier.notify_GrepCopyingFilter(project, message);
-			} else {
-				log.warn(message);
-			} 
+
+			if (previousIncompleteToken.get() != null) {
+				s = previousIncompleteToken.get() + s;
+				previousIncompleteToken.set(null);
+			}
+
+			Key stdout = ProcessOutputTypes.STDOUT;
+			if (type == ConsoleViewContentType.ERROR_OUTPUT) {
+				stdout = ProcessOutputTypes.STDERR;
+			} else if (type == ConsoleViewContentType.SYSTEM_OUTPUT) {
+				stdout = ProcessOutputTypes.SYSTEM;
+			}
+
+			String substring = profile.limitInputGrepLength_andCutNewLine(s);
+			CharSequence charSequence = profile.limitProcessingTime(substring);
+			try {
+				if (matcher.matches(charSequence)) {
+					if (!s.endsWith("\n")) {
+						s = s + "\n";
+					}
+					myProcessHandler.notifyTextAvailable(s, stdout);
+				}
+			} catch (ProcessCanceledException e) {
+				String message = "Grep to a subconsole took too long, aborting to prevent input freezing.\n"
+						+ "Consider changing following settings: 'Match only first N characters on each line' or 'Max processing time for a line'\n"
+						+ "Matcher: " + matcher + "\n" + "Line: " + Utils.toNiceLineForLog(substring);
+				if (showLimitNotification) {
+					showLimitNotification = false;
+					Notifier.notify_GrepCopyingFilter(project, message);
+				} else {
+					log.warn(message);
+				}
+			}
+
 		}
+
 	}
 
 	@Override
