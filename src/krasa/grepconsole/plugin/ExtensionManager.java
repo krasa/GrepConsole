28a70fb0cd73b186ed49e1e96316a5039a4f5cf5
Vojtěch Krása <vojta.krasa@gmail.com>
2018-05-14 09:51:12 +0200

    thread safety

--- src/krasa/grepconsole/plugin/ExtensionManager.java
+++ src/krasa/grepconsole/plugin/ExtensionManager.java
@@ -3,12 +3,12 @@ package krasa.grepconsole.plugin;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 
 public class ExtensionManager {
-	public static Map<String, Function<String, String>> functions = new HashMap<>();
+	public static Map<String, Function<String, String>> functions = new ConcurrentHashMap<>();
 
 	/**
 	 * Function: return null to remove a line, or return text which will be printed to console
