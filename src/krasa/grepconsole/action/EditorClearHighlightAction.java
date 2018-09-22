803eeba8225a29894d0a6291f157dbcaa2385445
Vojtěch Krása <vojta.krasa@gmail.com>
2018-09-23 01:16:18 +0200

    EditorClearHighlightAction - enable  only in editor

--- src/krasa/grepconsole/action/EditorClearHighlightAction.java
+++ src/krasa/grepconsole/action/EditorClearHighlightAction.java
@@ -3,6 +3,7 @@ package krasa.grepconsole.action;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
 import krasa.grepconsole.plugin.GrepConsoleApplicationComponent;
 import krasa.grepconsole.plugin.PluginState;
 
@@ -19,10 +20,18 @@ public class EditorClearHighlightAction extends HighlightManipulationAction {
 	public void actionPerformed(AnActionEvent e) {
 		PluginState pluginState = GrepConsoleApplicationComponent.getInstance().getState();
 		pluginState.getDonationNagger().actionExecuted();
-		
+
 		Editor editor = e.getData(PlatformDataKeys.EDITOR);
 		if (editor != null) {
 			editor.getMarkupModel().removeAllHighlighters();
 		}
+	}
+
+	@Override
+	public void update(AnActionEvent e) {
+		super.update(e);
+		Project project = e.getProject();
+		Editor editor = e.getData(PlatformDataKeys.EDITOR);
+		e.getPresentation().setEnabled(project != null && editor != null);
 	}
 }
