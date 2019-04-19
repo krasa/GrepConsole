2e81b428812bf52cc4c03062e7895ebe747787f3
Vojtěch Krása <vojta.krasa@gmail.com>
2019-04-19 21:47:04 +0200

    Major GrepInputFilter rewrite - CLion pushes multiple lines to InputFilter at once - related to #55

--- src/krasa/grepconsole/filter/GrepInputFilter.java
+++ src/krasa/grepconsole/filter/GrepInputFilter.java
@@ -3,6 +3,8 @@ package krasa.grepconsole.filter;
 import com.intellij.execution.filters.InputFilter;
 import com.intellij.execution.ui.ConsoleView;
 import com.intellij.execution.ui.ConsoleViewContentType;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.progress.ProcessCanceledException;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Pair;
 import krasa.grepconsole.filter.support.FilterState;
@@ -11,16 +13,20 @@ import krasa.grepconsole.model.GrepExpressionItem;
 import krasa.grepconsole.model.Profile;
 import krasa.grepconsole.plugin.ExtensionManager;
 import krasa.grepconsole.utils.Notifier;
+import krasa.grepconsole.utils.Utils;
 import org.apache.commons.lang.StringUtils;
+import org.jetbrains.annotations.Nullable;
 
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class GrepInputFilter extends AbstractGrepFilter implements InputFilter {
+	private static final Logger log = Logger.getInstance(GrepInputFilter.class);
 
-	private static final List<Pair<String, ConsoleViewContentType>> REMOVE_OUTPUT = Collections.singletonList(new Pair<>(null, null));
+	private static final Pair<String, ConsoleViewContentType> REMOVE_OUTPUT_PAIR = new Pair<>(null, null);
+	private static final Pattern PATTERN = Pattern.compile("(?<=\n)");
 
 	private WeakReference<ConsoleView> console;
 	private volatile boolean lastLineFiltered = false;
@@ -42,70 +48,127 @@ public class GrepInputFilter extends AbstractGrepFilter implements InputFilter {
 		ConsoleView consoleView = console.get();
 		if (consoleView != null) {
 			testConsole = consoleView.getClass().getName().startsWith("com.intellij.execution.testframework.ui");
+			log.info("Initializing for " + consoleView.getClass().getName());
 		}
 	}
 
 	@Override
-	public List<Pair<String, ConsoleViewContentType>> applyFilter(String s, ConsoleViewContentType consoleViewContentType) {
-		if (lastLineFiltered && removeNextNewLine && s.equals("\n")) {
-			removeNextNewLine = false;
-			return REMOVE_OUTPUT;
+	public List<Pair<String, ConsoleViewContentType>> applyFilter(String text, ConsoleViewContentType consoleViewContentType) {
+		List<Pair<String, ConsoleViewContentType>> result = filter(text, consoleViewContentType);
+
+		grep(result, text, consoleViewContentType);
+
+		return result;
+	}
+
+	@Nullable
+	protected List<Pair<String, ConsoleViewContentType>> filter(String text, ConsoleViewContentType consoleViewContentType) {
+		List<Pair<String, ConsoleViewContentType>> result = null;
+		if (!grepProcessors.isEmpty()) {
+			String[] split;
+			try {
+				split = PATTERN.split(profile.limitProcessingTime(text), 0);
+			} catch (ProcessCanceledException e) {
+				notifyError(text);
+				return null;
+			}
+
+			result = new ArrayList<>(split.length);
+
+			for (int i = 0; i < split.length; i++) {
+				String s = split[i];
+				if (lastLineFiltered && removeNextNewLine && s.equals("\n")) {
+					removeNextNewLine = false;
+					result.add(REMOVE_OUTPUT_PAIR);
+					continue;
+				}
+
+
+				FilterState state = super.filter(s, -1);
+
+				clearConsole(state, result, consoleViewContentType);
+
+				prepareResult(result, s, state, consoleViewContentType);
+
+			}
+
+			//let other InputFilters work
+			if (split.length == 1 && result.size() == 1) {
+				if (result.get(0).first == text) {
+					result = null;
+				}
+			}
 		}
 
-		FilterState state = super.filter(s, -1);
+		return result;
+	}
 
-		clearConsole(state);
-
-		List<Pair<String, ConsoleViewContentType>> pairs = prepareResult(state, consoleViewContentType);
-
-
-		grep(pairs, s, consoleViewContentType);
-
-		return pairs;
+	private void notifyError(String text) {
+		String message = "preparing input text for matching took too long, aborting to prevent GUI freezing.\n"
+				+ "Consider reporting bug, not logging huge chunks of text, or changing following settings: 'Max processing time for a line'\n"
+				+ "Text: " + Utils.toNiceLineForLog(text);
+		if (showLimitNotification) {
+			showLimitNotification = false;
+			Notifier.notify_InputAndHighlight(project, message);
+		} else {
+			log.warn(message);
+		}
 	}
 
 	//if we change output, then next InputFilter won't get processed, so Grep it here.
-	private void grep(List<Pair<String, ConsoleViewContentType>> pairs, String s, ConsoleViewContentType consoleViewContentType) {
+	private void grep(List<Pair<String, ConsoleViewContentType>> pairs, String text, ConsoleViewContentType consoleViewContentType) {
 		if (grepFilter != null) {
-			if (pairs != REMOVE_OUTPUT) {
-				if (pairs != null) {
-					Pair<String, ConsoleViewContentType> pair = pairs.get(0);
-					grepFilter.applyFilter(pair.first, pair.second);
-				} else {
-					grepFilter.applyFilter(s, consoleViewContentType);
+			if (pairs == null) {
+				grepFilter.applyFilter(text, consoleViewContentType);
+			} else {
+				for (int i = 0; i < pairs.size(); i++) {
+					Pair<String, ConsoleViewContentType> pair = pairs.get(i);
+					if (pair == REMOVE_OUTPUT_PAIR) {
+					} else if (pair != null) {
+						grepFilter.applyFilter(pair.first, pair.second);
+					}
 				}
 			}
 		}
 	}
 
-	public void clearConsole(FilterState state) {
+	public void clearConsole(FilterState state, List<Pair<String, ConsoleViewContentType>> pairs, ConsoleViewContentType consoleViewContentType) {
 		if (state != null && state.isClearConsole()) {
 			ConsoleView consoleView = console.get();
 			if (consoleView != null) {
 				consoleView.clear();
+
+				grep(pairs, null, consoleViewContentType);
+				pairs.clear();
 			}
 		}
 	}
 
-	private List<Pair<String, ConsoleViewContentType>> prepareResult(FilterState state, ConsoleViewContentType consoleViewContentType) {
-		List<Pair<String, ConsoleViewContentType>> result = null;
+	private List<Pair<String, ConsoleViewContentType>> prepareResult(List<Pair<String, ConsoleViewContentType>> result, String s, FilterState state, ConsoleViewContentType consoleViewContentType) {
+		boolean previousLineFiltered = lastLineFiltered;
+		lastLineFiltered = false;
+		removeNextNewLine = false;
+
 		if (state != null) {
 			if (state.isExclude()) {
-				result = REMOVE_OUTPUT;
+				result.add(REMOVE_OUTPUT_PAIR);
 				lastLineFiltered = true;
-			} else if (profile.isMultilineInputFilter() && !state.isMatchesSomething() && lastLineFiltered) {
-				result = REMOVE_OUTPUT;
+				removeNextNewLine = testConsole && state.notTerminatedWithNewline();
+				return result;
+			} else if (profile.isMultilineInputFilter() && !state.isMatchesSomething() && previousLineFiltered) {
+				result.add(REMOVE_OUTPUT_PAIR);
+				lastLineFiltered = true;
+				removeNextNewLine = testConsole && state.notTerminatedWithNewline();
+				return result;
 			}
 		}
-		if (result == null) {
-			lastLineFiltered = false;
-			removeNextNewLine = false;
-			if (state != null && state.isTextChanged()) {
-				return Collections.singletonList(new Pair<>(state.getText(), consoleViewContentType));
-			}
-			return null;// input is not changed
+
+		if (state != null && state.isTextChanged()) {
+			result.add(new Pair<>(state.getText(), consoleViewContentType));
+			return result;
 		} else {
-			removeNextNewLine = testConsole && state.notTerminatedWithNewline();
+			// input is not changed
+			result.add(new Pair<>(s, consoleViewContentType));
 			return result;
 		}
 	}
