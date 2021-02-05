ebc52d63e9e8e844735c1968e3d3314848973543
Vojtěch Krása <vojta.krasa@gmail.com>
2021-02-05 09:58:00 +0100

    Fixes #186 Do not request resource from classloader using path with leading slash (path=/krasa/grepconsole/icons/donate.png)

--- src/krasa/grepconsole/gui/Donate.java
+++ src/krasa/grepconsole/gui/Donate.java
@@ -19,7 +19,7 @@ public class Donate {
 
 	public static void init(JPanel donatePanel, JButton donate) {
 		donate.setBorder(null);
-		donate.setIcon(IconLoader.getIcon("/krasa/grepconsole/icons/donate.png"));
+		donate.setIcon(IconLoader.getIcon("/krasa/grepconsole/icons/donate.png", Donate.class));
 		donate.setContentAreaFilled(false);
 		donate.addActionListener(new ActionListener() {
 			@Override
